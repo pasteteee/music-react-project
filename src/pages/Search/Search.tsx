c3b09acd55f2dbc5f376d2e5bc9af7cdda06ed57
ANREAL00 <anton.redko2626@gmail.com>
2025-10-31 00:43:53 +0300

    path indetefier

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,6 +1,14 @@
 import styles from "./Search.module.scss";
 import { Link } from "react-router-dom";
+import { PathIndetefier } from "../../components/PathIndetefier/PathIndetefier";
+import { usePathIndetefier } from "../../utils/PathIndetefier";
 
 export default function SearchPage() {
-  return <Link to="/">Home</Link>;
+  const pathIndetefier = usePathIndetefier();
+
+  return (
+    <div>
+      <PathIndetefier items={pathIndetefier} />
+    </div>
+  );
 }
---
+++ src/utils/PathIndetefier.ts
@@ -0,0 +1,35 @@
+import { useLocation } from "react-router-dom";
+
+export interface PathItem {
+    label: string;
+    path?: string;
+    isActive?: boolean;
+}
+
+export const usePathIndetefier = () => {
+    const location = useLocation();
+
+    const getPageName = (pathname: string): string => {
+        const path = pathname.replace('/', '');
+
+        if (path === '') return 'Discover';
+        if (path === 'search') return 'Search';
+
+        return path.split('-').map(word => word.charAt(0).toUpperCase + word.slice(1)).join(' ');
+    }
+
+    const currentPageName = getPageName(location.pathname);
+
+    let pathIndetefier: PathItem[] = [
+        { label: 'Discover', path: '/' }
+    ];
+
+    if (currentPageName !== 'Discover') {
+        pathIndetefier = [
+            { label: 'Discover', path: '/' },
+            { label: currentPageName, isActive: true }
+        ];
+    }
+
+    return pathIndetefier;
+}
